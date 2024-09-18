37fb1aaaffa0ab9012e7e29a1c66d05d275a7b17
Kunal-kankariya <kunalkankriya2203@gmail.com>
2024-09-18 16:50:15 +0530

    text: added env details, commands.js, login_logout test, and updated new_request test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,15 +24,14 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("loginWithCredentials", (username, passwrod) => {
+
+Cypress.Commands.add("loginWithCredentials", () => {
   cy.visit("/");
   cy.get('[data-testid="login-with-password"]').should("be.visible").click();
   cy.get('[data-testid="email"]')
-    .should("be.visible")
-    .type("qa+employee@carevalidate.com");
+    .should("be.visible").type(Cypress.env('username'))
   cy.get('[data-testid="password"]')
-    .should("be.visible")
-    .type("bLPgk5tr7D3ZqpXvV@aNKz");
+    .should("be.visible").type(Cypress.env('password'))
   cy.get('[data-testid="continue"]').should("be.enabled").click();
   cy.contains("QA employee (QA Interview)").should("exist");
 });
@@ -42,3 +41,35 @@ Cypress.Commands.add("logout", () => {
   cy.contains("Log out").click();
   cy.contains("Welcome back! Please enter your details.").should("exist");
 });
+
+Cypress.Commands.add("clickOptionWithElement",(element) =>{
+  cy.get(element).should("be.visible").click();
+})
+
+Cypress.Commands.add("clickOptionWithText",(locator)=>{
+  cy.contains(locator).should('be.visible').click();
+})
+
+Cypress.Commands.add("selectAcomodations",(text)=>{
+  cy.get(".q-item__section")
+      .contains(text)
+      .should("be.visible")
+      .click({ force: true });
+    cy.get("body").click();
+})
+
+Cypress.Commands.add("getElement",(element)=>{
+  return cy.get(element)
+})
+
+Cypress.Commands.add("ensureElementPresent",(locator)=>{
+  return cy.get(locator).should("be.visible");
+})
+
+Cypress.Commands.add("ensureTextPresent",(text)=>{
+  return cy.contains(text).should("be.visible").should('exist');
+})
+
+Cypress.Commands.add("ensureElementEnabled",(locator)=>{
+  cy.get(locator).should("be.enabled").click();
+})
